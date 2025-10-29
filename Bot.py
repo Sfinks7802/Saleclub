734739d74795458148f269ac8dbbe768f8465afe
Sfinks7802 <sfinks7802@gmail.com>
2025-10-29 20:49:06 +0300

    removed private mode

--- Bot.py
+++ Bot.py
@@ -5,5 +5,5 @@ from aiogram.client.default import DefaultBotProperties
 
 bot = Bot(
     token=os.environ.get("BotToken"),
-    default=DefaultBotProperties(parse_mode="HTML", protect_content=True),
+    default=DefaultBotProperties(parse_mode="HTML", protect_content=False),
 )
