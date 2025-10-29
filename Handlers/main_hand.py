72c6807a3f4eeaa596e8e78846de3f8f091db491
Sfinks7802 <sfinks7802@gmail.com>
2025-10-30 00:29:35 +0300

    used black .

--- Handlers/main_hand.py
+++ Handlers/main_hand.py
@@ -9,7 +9,7 @@ router = Router()
 async def cmd_start(message: types.Message):
     reviews = get_reviews_from_wb(message.text)
     if reviews[0]:
-        await message.answer(f'{reviews[1]}')
+        await message.answer(f"{reviews[1]}")
         return
     ans = analyze_reviews_with_gigachat(token=token, reviews=reviews[1])
     await message.answer(f"{ans}")
